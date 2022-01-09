f45ecf336375e49f88c26345d226c9450aa3bb36
Emma_Kincaid <111394951@umail.ucc.ie>
2022-01-09 13:17:20 +0000

    Splash screen implemented

--- app/src/main/java/ie/wit/assignment1/activities/SplashScreen.kt
+++ app/src/main/java/ie/wit/assignment1/activities/SplashScreen.kt
@@ -1,4 +1,29 @@
 package ie.wit.assignment1.activities
 
-class SplashScreen {
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import ie.wit.assignment1.R
+import ie.wit.assignment1.views.login.LoginView
+
+
+@Suppress("DEPRECATION")
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        Handler().postDelayed({
+            val intent = Intent(this, LoginView::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
 }
